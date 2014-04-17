9b6ecd4c3d9a05bdda87d09a915676874f0d6122
Wang Qilin <wangqilin129@gmail.com>
2014-04-17 16:41:26 +0800

    丧心病狂

--- printWithoutColon.c
+++ printWithoutColon.c
@@ -11,6 +11,14 @@ int main(int argc, char const *argv[])
 	}
 
 	//use while statement, it will print countless times
-	while (printf("hello world (while) \n") && 0) {//后面的‘&& 0’ 是伟哥加上去的，这也是我们叫他伟哥的原因
+	while (printf("hello world (while) \n") && 0) {	// "&& 0" was added by 伟哥
+													// so that this while-statement won't print countless times
 	}
+	
+	//above three methods work, because the printf statement returns the number of characters that was printed
+	
+	// 伟哥 suggested there is another way, which is to use the comma expression
+	// in comma expression, say A,B,C, the execution order is A->B->C, and finally returns the value of C
+	while(printf("hello world (comma expression)\n"), 0) {
+	} 
 }
